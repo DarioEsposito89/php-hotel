59c05288705cf5ee2c39522ede782faa47838045
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-07 16:46:00 +0200

    stampato array all'interno della tabella

--- index.php
+++ index.php
@@ -1,83 +1,85 @@
-<!-- HOTEL ARRAY -->
-<?php
-$hotels = [
-
-    // hotel 1 data
-    [
-        'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
-        'parking' => true,
-        'vote' => 4,
-        'distance_to_center' => 10.4
-    ],
-
-    // hotel 2 data
-    [
-        'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 2
-    ],
-
-    // hotel 3 data
-    [
-        'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
-        'parking' => false,
-        'vote' => 1,
-        'distance_to_center' => 1
-    ],
-
-    // hotel 4 data
-    [
-        'name' => 'Hotel Bellavista',
-        'description' => 'Hotel Bellavista Descrizione',
-        'parking' => false,
-        'vote' => 5,
-        'distance_to_center' => 5.5
-    ],
-
-    // hotel 5 data
-    [
-        'name' => 'Hotel Milano',
-        'description' => 'Hotel Milano Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 50
-    ],
-];
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+    <title>Document</title>
+    
+    <?php 
+        $hotels = [
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+        ];
+?>
 </head>
 
 <body>
-    <h1>HOTELS</h1>
-
-    <table class="table table-striped table-dark">
-
+    <table class="table">
         <thead>
             <tr>
-                <?php foreach ($hotels[0] as $key => $value) { ?>
-                    <th scope="col"><?php echo $key; ?></th>
-                <?php } ?>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance to center</th> 
             </tr>
         </thead>
 
+    <?php foreach($hotels as $key => $hotel){
+            $name = $hotel['name'];
+            $description = $hotel['description'];
+            $parking = $hotel['parking'];
+            $vote = $hotel['vote'];
+            $distance_to_center = $hotel['distance_to_center']; ?>
+
+            <tbody>
+                <tr>
+                    <th scope="row"><?php echo $key ?></th>
+                    <td><?php echo $name ?></td>
+                    <td><?php echo $description ?></td>
+                    <td><?php echo $parking === true ? "Yes" : "No" ?></td>
+                    <td><?php echo $vote . "/5 stars"  ?></td>
+                    <td><?php echo $distance_to_center . "Km" ?></td>
+                </tr>
+            </tbody>
+    <?php } ?>
+            </table>
 </body>
 </html>
